a281690527a14d207e8a6dda8c0e794eff3b7025
Cozma Andrei <cozma.andrei91@gmail.com>
2023-12-04 00:05:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,31 +12,36 @@ const grid = document.querySelector(".grid");
 const cells = document.querySelectorAll(".cell");
 const countGamesWonPlayer1 = document.querySelector(".player1");
 const countGamesWonPlayer2 = document.querySelector(".player2");
-let move = 1;
+let move = 0;
 let gamesWonPlayer1 = 0;
 let gamesWonPlayer2 = 0;
+let runningGame = true;
 
 fillTheCellWithXor0();
 
 function fillTheCellWithXor0() {
    for (let i = 0; i < cells.length; ++i) {
       let cell = cells[i];
-      cell.onclick = function () {
-         winningText.innerHTML = "";
-         if (cell.innerHTML == "") {
-            if (move % 2 == 1) {
-               cell.innerHTML = "X";
+      cell.addEventListener("click", () => {
+         if (cell.innerHTML == "" && runningGame) {
+            if (move % 2 == 0) {
+               cell.innerHTML = "X"; 
             } else {
                cell.innerHTML = "0";
             }
+            ++move;
+            noOneWon();
+            winningPlayerOne();
+            winningPlayerTwo()
+            countWins();
          }
-         ++move;
-         winningPlayerOne();
-         winningPlayerTwo()
-         noOneWon();
-         countWins();
-      };
+      });
+   }
+}
 
+function noOneWon() {
+   if (move == cells.length) {
+      winningText.innerHTML = "No WINNER";
    }
 }
 
@@ -44,35 +49,27 @@ function winningPlayerOne() {
    if (cell0.innerHTML == "X" && cell1.innerHTML == "X" &&
       cell2.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    } else if (cell3.innerHTML == "X" && cell4.innerHTML == "X" &&
       cell5.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    } else if (cell6.innerHTML == "X" && cell7.innerHTML == "X" &&
       cell8.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    } else if (cell0.innerHTML == "X" && cell3.innerHTML == "X" &&
       cell6.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    } else if (cell1.innerHTML == "X" && cell4.innerHTML == "X" &&
       cell7.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    } else if (cell2.innerHTML == "X" && cell5.innerHTML == "X" &&
       cell8.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    } else if (cell0.innerHTML == "X" && cell4.innerHTML == "X" &&
       cell8.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    } else if (cell2.innerHTML == "X" && cell4.innerHTML == "X" &&
       cell6.innerHTML == "X") {
       winningText.innerHTML = "Player 1 is the WINNER";
-      emptyTheCell();
    }
 }
 
@@ -80,42 +77,27 @@ function winningPlayerTwo() {
    if (cell0.innerHTML == "0" && cell1.innerHTML == "0" &&
       cell2.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
    } else if (cell3.innerHTML == "0" && cell4.innerHTML == "0" &&
       cell5.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
    } else if (cell6.innerHTML == "0" && cell7.innerHTML == "0" &&
       cell8.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
    } else if (cell0.innerHTML == "0" && cell3.innerHTML == "0" &&
       cell6.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
    } else if (cell1.innerHTML == "0" && cell4.innerHTML == "0" &&
       cell7.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
    } else if (cell2.innerHTML == "0" && cell5.innerHTML == "0" &&
       cell8.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
    } else if (cell0.innerHTML == "0" && cell4.innerHTML == "0" &&
       cell8.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
    } else if (cell2.innerHTML == "0" && cell4.innerHTML == "0" &&
       cell6.innerHTML == "0") {
       winningText.innerHTML = "Player 2 is the WINNER";
-      emptyTheCell();
-   }
-}
-
-function noOneWon() {
-   if (move == cells.length + 1) {
-      winningText.innerHTML = "No WINNER";
-      emptyTheCell();
    }
 }
 
@@ -123,21 +105,24 @@ function emptyTheCell() {
    for (let i = 0; i < cells.length; ++i) {
       let cell = cells[i];
       cell.innerHTML = "";
-      move = 1;
+      move = 0;
    }
 }
 
 function restartButton() {
    emptyTheCell();
    winningText.innerHTML = "";
+   runningGame = true;
 }
 
 function countWins() {
    if (winningText.innerHTML == "Player 1 is the WINNER") {
       ++gamesWonPlayer1;
       countGamesWonPlayer1.innerHTML = `Player 1: X || Games won: ${gamesWonPlayer1}`;
+      runningGame = false;
    } else if (winningText.innerHTML == "Player 2 is the WINNER") {
       ++gamesWonPlayer2;
       countGamesWonPlayer2.innerHTML = `Player 2: 0 || Games won: ${gamesWonPlayer2}`;
+      runningGame = false;
    }
 }
